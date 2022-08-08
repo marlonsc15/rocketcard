7af21d83ee9bea8c2d9d2dc13c002aea771acb81
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-08-08 17:29:33 -0300

    Função de mudança de cor

---
+++ script.js
@@ -0,0 +1,15 @@
+function customizar() {
+    let r = Math.floor(Math.random()* 255);
+    let g = Math.floor(Math.random()* 255);
+    let b = Math.floor(Math.random()* 255);
+    let a = Math.floor(Math.random()* 255);
+
+        document.getElementById("card").style.border = "20px solid rgb("+r+","+g+","+b+")";
+
+        document.getElementById("img").style.border = "3px solid rgb("+r+","+g+","+b+")";
+
+        document.getElementById("nome").style.color = "rgb("+r+","+g+","+b+")";
+
+        document.getElementById("background").style.background = "rgba("+r+","+g+","+b+",0.300)";
+
+}
