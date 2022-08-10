1063282c986dd188868e65262e63abe9866ee70a
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-08-10 16:30:38 -0300

    adição da função relacionada a api do github

--- script.js
+++ script.js
@@ -14,3 +14,20 @@ function customizar() {
 
         document.getElementById("image-border").style.border = "1px solid rgb("+r+","+g+","+b+")"
 }
+
+const url = "https://api.github.com/users/marlonsc15";
+
+function getApiGitHub(){
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        following.textContent = data.following+' following';
+        followers.textContent = data.followers+' followers';
+        repos.textContent = data.public_repos+' repositorios';
+        company.textContent = data.company;
+        local.textContent = data.location;
+    })
+    .catch(error => console.error(error))
+}
+
+getApiGitHub()
